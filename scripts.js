e81c49f9c424717930748ed6c1a080298fdb3761
Robson Flávio Freitas <robson.flavio20@outlook.com>
2021-02-04 19:31:36 -0300

    inserção de scripts no html relacionado à tabelas

---
+++ scripts.js
@@ -0,0 +1,78 @@
+const Modal = {
+    open (){
+        document
+        .querySelector('.modal-overlay')
+        .classList.add('active')
+
+    },
+
+    close(){
+        document
+        .querySelector('.modal-overlay')
+        .classList
+        .remove('active')
+
+    }
+}
+
+const transactions= [
+        {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+        },
+        {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021',
+        },
+        {
+        id: 3,
+        description: "Internet",
+        amount: -20000,
+        date: '23/01/2021',
+        },
+    ]
+
+const Transaction ={
+    incomes(){
+
+    },
+    expenses(){
+
+    },
+
+    total(){
+
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index){
+        
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+        console.log(DOM.transactionsContainer)
+
+    },
+    innerHTMLTransaction(transaction) {
+        const html=`
+            <td class="description">${transaction.description}</td>
+            <td class="expense">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+            `
+            return html
+    }
+
+}
+
+DOM.addTransaction(transactions[1])
